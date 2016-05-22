d61a3d1b5fe4abfd1ff3894b7a089a42e2825d60
Nick Johnstone <ncwjohnstone@gmail.com>
2016-05-23 10:18:48 +1200

    Add tests for add, remove and Collection constructor

---
+++ test/collection-test.js
@@ -0,0 +1,75 @@
+/* globals describe, it */
+import assert from 'assert';
+import xs from 'xstream';
+import Collection from '../src/collection';
+
+function Widget ({props$}) {
+  return {
+    state$: props$
+  };
+}
+
+describe('Collection', () => {
+  const collection = Collection(Widget);
+
+  it('takes an object of sources to pass to each item', () => {
+    const props$ = xs.empty();
+
+    const collection = Collection(Widget, {props$});
+
+    assert.equal(collection.add().asArray()[0].state$, props$);
+  });
+
+  describe('.add', () => {
+    it('starts out empty', () => {
+      assert.equal(collection.asArray().length, 0);
+    });
+
+    it('adds items', () => {
+      assert.equal(collection.add().asArray().length, 1);
+    });
+
+    it('is immutable', () => {
+      const collectionWithOneItem = collection.add();
+
+      assert.equal(collectionWithOneItem.asArray().length, 1);
+
+      collectionWithOneItem.add();
+      collectionWithOneItem.add();
+      collectionWithOneItem.add();
+
+      assert.equal(collectionWithOneItem.asArray().length, 1);
+      assert.equal(collectionWithOneItem.add().asArray().length, 2);
+    });
+
+    it('takes an object of additional sources to be passed to the item', () => {
+      const props$ = xs.empty();
+
+      const collectionWithOneItemWithProps = collection.add({props$});
+
+      assert.equal(props$, collectionWithOneItemWithProps.asArray()[0].state$);
+    });
+  });
+
+  describe('.remove', () => {
+    it('removes the given item from the collection', () => {
+      const collectionWithOneItem = collection.add();
+
+      const item = collectionWithOneItem.asArray()[0];
+
+      const emptyCollection = collectionWithOneItem.remove(item);
+
+      assert.equal(emptyCollection.asArray().length, 0);
+    });
+
+    it("doesn't explode if the item isn't present", () => {
+      const collectionWithOneItem = collection.add();
+
+      const item = collectionWithOneItem.asArray()[0];
+
+      collectionWithOneItem
+        .remove(item)
+        .remove(item);
+    });
+  });
+});

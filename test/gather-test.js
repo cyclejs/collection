081f311da2add9ab3a13e8117b120f93e90cc296
hypnos <hypnos@yandex-team.ru>
2016-06-09 14:23:09 +0300

    Collection.gather test for removing

--- test/gather-test.js
+++ test/gather-test.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import xs from 'xstream';
+import delay from 'xstream/extra/delay';
 import Collection from '../src/collection';
 
 function Widget ({props}) {
@@ -110,6 +111,55 @@ describe('Collection.gather', (done) => {
       [
         [{foo: 'bar'}],
         [{baz: 'quix'}]
+      ]
+    ];
+
+    collection$.take(expected.length).addListener({
+      next (collection) {
+        const expectedItems = expected.shift();
+        const items = collection.asArray();
+        assert.equal(items.length, expectedItems.length);
+
+        items.forEach((item, i) => {
+          const expectedItem = expectedItems[i];
+          item.state$.take(expectedItem.length).addListener({
+            next (val) {
+              assert.deepEqual(val, expectedItem.shift());
+            },
+            error (err) {done(err)},
+            complete () {}
+          });
+        });
+      },
+      error (err) {done(err)},
+      complete () {
+        done();
+      }
+    });
+  });
+});
+
+describe('Collection.gather', (done) => {
+  it('removes the items that are no more present', (done) => {
+    const itemState$ = xs.of([
+        { id: 0, props: {foo: 'bar'}},
+        { id: 1, props: {baz: 'quix'}}
+      ],
+      [
+        { id: 0, props: {foo: 'bar'}}
+      ])
+      // items should be added asynchronously for collection.reducers to work properly
+      .compose(delay());
+    const collection$ = Collection.gather(itemState$, Widget, {}, {});
+
+    const expected = [
+      [],
+      [
+        [{foo: 'bar'}],
+        [{baz: 'quix'}]
+      ],
+      [
+        [{foo: 'bar'}]
       ]
     ];
 

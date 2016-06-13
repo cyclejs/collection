410428fc52ce747d1978fb9f2c3d380792d72a1f
hypnos <hypnos@yandex-team.ru>
2016-06-13 20:17:30 +0300

    Collection.gather refactoring

--- src/collection.js
+++ src/collection.js
@@ -124,82 +124,91 @@ Collection.pluck = function pluck (collection$, sinkProperty) {
 
 // convert a stream of items' sources snapshots into a stream of collections
 Collection.gather = function gather (itemsState$ , component, sources, handlers = {}, idAttribute = 'id') {
-  const makeDestroyable = component => (sources) => {
-    const sinks = component(sources);
-    return {
-      ...sinks,
-      remove$: xs.merge(sinks.remove$ || xs.never(), sources.destroy$)
+  function makeDestroyable (component) {
+    return (sources) => {
+      const sinks = component(sources);
+      return {
+        ...sinks,
+        remove$: xs.merge(sinks.remove$ || xs.never(), sources.destroy$)
+      };
     };
-  };
+  }
+
+  // finds items not present in previous snapshot
+  function findNewItems({prevIds}, items) {
+    return {
+      prevIds: items.map(item => item[idAttribute]),
+      addedItems: items.filter(item => prevIds.indexOf(item[idAttribute]) === -1)
+    };
+  }
+
+  function compareJSON(value, nextValue) {
+    if (value === nextValue) {
+      return true;
+    }
+    try {
+      if (JSON.stringify(value) === JSON.stringify(nextValue)) {
+        return true;
+      }
+    } catch(e) {}
+    // if not equal or not serializable
+    return false;
+  }
+
+  // turn a new item into a hash of source streams, tracking all the future updates
+  function itemToSourceStreams(addedItem) {
+    const itemStateInfinite$ = itemsState$
+      .map(items =>
+        items.find(item => item[idAttribute] === addedItem[idAttribute])
+      );
+    // if an item isn't present if a new snapshot, it shall be destroyed
+    const destroy$ = itemStateInfinite$.filter(item => !item).take(1);
+    const itemState$ = itemStateInfinite$.endWhen(destroy$);
+
+    return Object.keys(addedItem)
+      .reduce((sources, key) => {
+        // skip idAttribute
+        if (key === idAttribute) {
+          return sources;
+        }
+
+        return {
+          ...sources,
+          [key]: itemState$
+            .map(state => state[key])
+            .startWith(addedItem[key])
+            // skip the snapshot if the value didn't change
+            .compose(dropRepeats(compareJSON))
+            .remember()
+        };
+      }, {
+        destroy$
+      });
+  }
+
+  function addAllItems(itemsSources) {
+    return collection =>
+      itemsSources.reduce(
+        (collection, sources) => collection.add(sources),
+        collection
+      );
+  }
+
   const collection = Collection(makeDestroyable(component), sources, {
     remove$: (collection, item) => collection.remove(item),
     ...handlers
   });
-  // each time a new item appears, it should be added to the collection
+
   const addReducers$ = itemsState$
     // get the added items at each step
-    .fold(
-      ({prevIds}, items) => ({
-        prevIds: items.map(item => item[idAttribute]),
-        addedItems: items.filter(item => prevIds.indexOf(item[idAttribute]) === -1)
-      }),
-      {
-        prevIds: [],
-        addedItems: []
-      }
-    )
+    .fold(findNewItems, {prevIds: [], addedItems: []})
     .map(({addedItems}) => addedItems)
     .filter(addedItems => addedItems.length)
-    // turn each new item into a hash of source streams, tracking all the future updates
-    .map(addedItems =>
-      addedItems.map(addedItem => {
-        const itemStateInfinite$ = itemsState$
-          .map(items =>
-            items.find(item => item[idAttribute] === addedItem[idAttribute])
-          );
-        // if an item isn't present if a new snapshot, it shall be destroyed
-        const destroy$ = itemStateInfinite$.filter(item => !item).take(1);
-        const itemState$ = itemStateInfinite$.endWhen(destroy$);
-        
-        return Object.keys(addedItem)
-          .reduce((sources, key) => {
-            if (key === idAttribute) {
-              return sources;
-            }
+    .map(addedItems => addedItems.map(itemToSourceStreams))
+    .map(addAllItems);
 
-            return {
-              ...sources,
-              [key]: itemState$
-                .map(state => state[key])
-                .startWith(addedItem[key])
-                // skip the snapshot if the value didn't change
-                .compose(dropRepeats((value, nextValue) => {
-                  if (value === nextValue) {
-                    return true;
-                  }
-                  try {
-                    if (JSON.stringify(value) === JSON.stringify(nextValue)) {
-                      return true;
-                    }
-                  } catch(e) {}
-                  // if not equal or not serializable
-                  return false;
-                }))
-                .remember()
-            };
-          }, {
-            destroy$
-          })
-      })
-    )
-    .map(itemsSources => collection =>
-      itemsSources.reduce(
-        (collection, sources) => collection.add(sources),
-        collection
-      )
-    );
   return xs.merge(addReducers$, collection.reducers)
-      .fold((collection, reducer) => reducer(collection), collection);
+    .fold((collection, reducer) => reducer(collection), collection);
 };
 
 export default Collection;
